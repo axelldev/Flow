32eeb7f51942e4a4e13e8ed1dd6132d5bf085918
axelldev <axellsolisrubio@gmail.com>
2025-08-09 03:40:46 -0600

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by axell solis on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
